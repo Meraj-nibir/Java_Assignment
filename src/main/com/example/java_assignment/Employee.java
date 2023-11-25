0002030a8dbb90702c510357853113c64a19b647
Meraj-UR-Rahman Nibir <75033388+Meraj-nibir@users.noreply.github.com>
2023-11-25 18:56:36 +0600

    assignment complete

---
+++ src/main/com/example/java_assignment/Employee.java
@@ -0,0 +1,13 @@
+package main.com.example.java_assignment;
+
+public class Employee {
+    String type;
+    int vacationLeave;
+    int sickLeave;
+
+    public Employee(String type, int vacationLeave, int sickLeave) {
+        this.type = type;
+        this.vacationLeave = vacationLeave;
+        this.sickLeave = sickLeave;
+    }
+}
